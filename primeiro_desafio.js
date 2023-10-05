fc4f8c8ed05f2ef6e754f08020316c7b092346e5
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-10-05 12:07:04 -0300

    Add files via upload

---
+++ primeiro_desafio.js
@@ -0,0 +1,65 @@
+//Atividade 1:
+//ao clicar no botão "Contagem de Cliques, incrementar de 1 em 1 o valor de uma variável"
+//mostrar o resultado no "resultado1"
+let clicks = 0
+function contagemClicks(){
+    clicks++;
+    resultado1 = document.getElementById("resultado1").textContent = clicks
+}
+
+//Atividade 2:
+//ao clicar no botão mostre o resultado de um número aleatório
+
+
+
+
+
+//Atividade 3:
+//ao clicar no botão mostre um texto(string), pode ser qualquer texto "oi tudo bem" por exemplo
+
+
+
+
+//Atividade 4:
+//ao clicar no botão, altere a cor do texto
+//extra: também alterar a cor do próprio botão
+
+
+
+
+
+//Atividade 5:
+// ao clicar no botão realizar a soma dos números inseridos nos inputs
+
+
+
+
+
+//Atividade 6:
+// ao clicar no botão realizar a multiplicação dos números inseridos nos inputs
+
+
+
+
+
+
+//Atividade 7:
+//verificar qual número é maior ou menor, utilizando os numeros inseridos nos inputs
+//essa atividade é mais desafiadora, terá que usar um if else
+//tente ao máximo fazer sozinha, quando nao souber mais pode pedir ajuda
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Atividade 8:
+//ao clicar no botão mostrar o resultado (da fruta) do que a pessoa escolheu
+//dica: usar apenas um nome de função usando parametros
